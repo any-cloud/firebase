ec968ce148196babfdd6979b68ac2a816ae1d394
Bryan Goldstein <brysgo@gmail.com>
2019-09-15 16:02:27 -0400

    write deploy script (doesn't work yet)

---
+++ src/cli/deploy.js
@@ -0,0 +1,43 @@
+const path = require("path");
+console.log("foo");
+const { spawn, spawnSync } = require("child_process");
+console.log("bar");
+const { cron: appCron, workers: appWorkers } = require(process.cwd());
+console.log("baz");
+
+export default {
+  command: "deploy",
+  aliases: [],
+  desc: "deploy your application to firebase",
+  builder: yargs => {},
+  handler: argv => {
+    console.log("here");
+    if (appWorkers) {
+      console.log("worker queue not implemented for firebase yet");
+    }
+    if (appCron) {
+      console.log("cron not implemented for firebase yet");
+    }
+    const args = ["deploy", ...process.argv.slice(3)];
+    const newCwd = path.join(__dirname, "../../");
+    const appCodeLinkPath = path.join(newCwd, "AC_APPLICATION_CODE");
+    console.log("clearing app code stage");
+    spawnSync("rm", ["-rf", appCodeLinkPath]);
+    console.log("copying new app code to stage");
+    spawnSync("cp", ["-R", process.cwd(), appCodeLinkPath]);
+    console.log("deploying with firebase");
+    const child = spawn(
+      require.resolve("firebase-tools/lib/bin/firebase"),
+      args,
+      { cwd: newCwd, env: process.env }
+    );
+
+    child.stdout.pipe(process.stdout);
+    child.stderr.pipe(process.stderr);
+
+    child.on("close", code => {
+      console.log(`firebase process exited with code ${code}`);
+      process.exit(0);
+    });
+  }
+};
