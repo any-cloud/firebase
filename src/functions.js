beb2227c55259e3d9efccfd5fc522532b23146ae
Bryan Goldstein <brysgo@gmail.com>
2019-09-18 13:36:45 -0400

    register cron functions with gcloud pubsub service

--- src/functions.js
+++ src/functions.js
@@ -1,8 +1,14 @@
-import { https } from "firebase-functions";
-import { http } from "@any-cloud/core";
+import { https, pubsub } from "firebase-functions";
+import { http, cron } from "@any-cloud/core";
 
 const server = http();
 
 // Graphql api
 // https://us-central1-<project-name>.cloudfunctions.net/api/
 export const api = https.onRequest(server);
+
+Object.keys(cron).forEach(cronJobName => {
+  const cronJob = cron[cronJobName];
+  const [cronSpec, cronFn] = cronJob;
+  module.exports[cronJobName] = pubsub.schedule(cronSpec).onRun(cronFn);
+});
